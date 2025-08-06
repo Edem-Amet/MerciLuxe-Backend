d8748adac4e47ba0ca6ba8fed5d5d7c969cd3e2f
Edem Amet <edemamet18@gmail.com>
2025-08-06 08:47:30 +0000

    Made all capital

--- routes/HeroRoutes.js
+++ routes/HeroRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const heroController = require('../controllers/heroController');
+const heroController = require('../controllers/HeroController');
 const { upload, uploadToCloudinary } = require('../middleware/HeroUploadMiddleware');
 
 // Error handler middleware
