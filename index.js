5495896b2b6aaabe66b6b26a1edff1a0b16eadb7
Edem Amet <edemamet18@gmail.com>
2025-10-24 06:21:05 +0000

    index cors change

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ const defaultOrigins = [
     'http://172.20.10.4:5173', // Your current mobile IP
     'https://merciluxe-frontend.onrender.com',
     'https://www.merciluxegift.store',
-    'http://192.168.1.181:5173', // Allow all devices on local network
-    'http://192.168.6.65:5173', // Common home network range
-    'http://[::1]'   // IPv6 localhost
 ];
 
 const allowedOrigins = [...new Set([
